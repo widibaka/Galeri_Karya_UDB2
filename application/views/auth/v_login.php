1de17731d4ca5a84cc615db0099e4beef9e52dd1
widibaka <widi_dwi@fikom.udb.ac.id>
2021-10-20 18:54:51 +0700

    lupa ditambah fikom

--- application/views/auth/v_login.php
+++ application/views/auth/v_login.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=0.8">
-  <title>Galeri Karya UDB | Log in</title>
+  <title>Galeri Karya Fikom UDB | Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
  <!--  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> -->
